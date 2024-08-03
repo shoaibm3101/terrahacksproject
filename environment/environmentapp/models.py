b5057748ea20f3d507f3b265ae81db541f671cfc
Shoaib Mustafa <shoaibmtoronto@gmail.com>
2024-08-03 16:09:21 -0400

    Adding Question and Company in Database

--- environment/environmentapp/models.py
+++ environment/environmentapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.question
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    score = models.IntegerField()
+    review = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}: {self.score}"
