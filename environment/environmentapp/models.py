69b9138ba2c33768d12cfb80b5f32d290d12fa70
Shoaib Mustafa <shoaibmtoronto@gmail.com>
2024-08-03 17:12:25 -0400

    Updating Questions Model

--- environment/environmentapp/models.py
+++ environment/environmentapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question = models.CharField(max_length=500)
+    score_worth = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question
